53d5d63357b86e5a1ffa2921501d8bfb75c777eb
akashhsr6ki46sujzer5ejez5jzr <159771615+akashhsr6ki46sujzer5ejez5jzr@users.noreply.github.com>
2024-02-12 16:22:10 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -56,7 +56,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('http://natgeo-gifs.tumblr.com/post/30410481004/via-l-ctea-at-different-times-of-the-day-in-a')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
         .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
